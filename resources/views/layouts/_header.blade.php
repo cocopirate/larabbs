45adbdeb08829cecdabf80e188605fabc4e4e999
DJDLee <l353861350@163.com>
2018-03-10 04:19:21 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-static-top navbar-light bg-light">
+    <div class="container">
+        <!-- Branding Image -->
+        <a class="navbar-brand" href="#">
+            <img src="https://v4.bootcss.com/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+        </a>
+
+        <!-- Collapsed Hamburger -->
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#app-navbar-collapse" aria-controls="app-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">社区<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">文档</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">问答</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <!-- Authentication Links -->
+                <a class="nav-item nav-link" href="#">登录</a>
+                <a class="nav-item nav-link" href="#">注册</a>
+            </ul>
+        </div>
+
+    </div>
+</nav>
