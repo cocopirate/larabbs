e91bd8169e6e6950051aed5d187d7fe67a405a49
DJDLee <l353861350@163.com>
2018-04-07 10:09:12 +0000

    User can upload topic image

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Requests\TopicRequest;
+use App\Handlers\ImageUploadHandler;
 use Auth;
 
 class TopicsController extends Controller
@@ -62,4 +63,28 @@ class TopicsController extends Controller
 
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
 	}
+
+    public function uploadImage(Request $request, ImageUploadHandler $uploader){
+
+        // 初始化返回数据，默认是失败的
+        $data = [
+            'success' => false,
+            'msg' => '上传失败',
+            'file_path' => ''
+        ];
+
+        // 判断是否有上传文件，并赋值给 $file
+        if($file = $request->upload_file){
+            //存储文件
+            $result = $uploader->save($file, 'topics', Auth::id(), 1024);
+
+            // 图片保存成功
+            if($result){
+                $data['file_path'] = $result['path'];
+                $data['msg'] = '上传成功';
+                $data['success'] = true;
+            }
+        }
+        return $data;
+    }
 }
