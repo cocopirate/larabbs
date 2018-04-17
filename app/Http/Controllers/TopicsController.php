4b06348a715f6442df475fe97483aa889d5dc017
DJDLee <l353861350@163.com>
2018-04-17 06:50:11 +0000

    Sidebar links

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -17,12 +18,12 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic, User $user)
+	public function index(Request $request, Topic $topic, User $user, Link $link)
 	{
 	    $topics = $topic->withOrder($request->order)->paginate(20);
 	    $active_users = $user->getActiveUsers();
-	    //dd($active_users);
-		return view('topics.index', compact('topics', 'active_users'));
+	    $links = $link->getAllCache();
+		return view('topics.index', compact('topics', 'active_users', 'links'));
 	}
 
     public function show(Request $request, Topic $topic)
--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -12,9 +12,23 @@
             <div class="text-center">活跃用户</div>
             <hr />
             @foreach($active_users as $active_user)
-                <a class="d-block" href="{{ route('users.show', $active_user->id) }}">
+                <a class="d-block" href="{{ route('users.show', $active_user->id) }}" target="_blank">
                     <img src="{{ $active_user->avatar }}" style="width: 24px;height: 24px;" />
                     <span>{{ $active_user->name }}</span>
+                </a>
+            @endforeach
+        </div>
+    </div>
+@endif
+
+@if(count($links))
+    <div class="card active-users">
+        <div class="card-body">
+            <div class="text-center">资源推荐</div>
+            <hr />
+            @foreach($links as $link)
+                <a class="d-block" href="{{ $link->link }}" target="_blank">
+                    <span>{{ $link->title }}</span>
                 </a>
             @endforeach
         </div>
