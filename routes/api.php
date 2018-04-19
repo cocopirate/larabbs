494391fc5c377a8c4e6aaf154c8bccc09c91fb31
DJDLee <l353861350@163.com>
2018-04-20 06:39:54 +0800

    get topics

--- routes/api.php
+++ routes/api.php
@@ -29,21 +29,27 @@ $api->version('v1', [
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
             ->name('api.verificationCodes.store');
+
         // 用户注册
         $api->post('users', 'UsersController@store')
             ->name('api.users.store');
+
         // 图片验证码
         $api->post('captchas', 'CaptchasController@store')
             ->name('api.captchas.store');
+
         // 第三方登录
         $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
             ->name('api.socials.authorizations.store');
+
         // 登录
         $api->post('authorizations', 'AuthorizationsController@store')
             ->name('api.authorizations.store');
+
         // 刷新token
         $api->put('authorizations/current', 'AuthorizationsController@update')
             ->name('api.authorizations.update');
+
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
@@ -56,31 +62,49 @@ $api->version('v1', [
         'expires' => config('api.rate_limits.access.expires'),
     ], function ($api){
         // 游客可以访问的接口
+
+        // 获取分类列表
         $api->get('categories', 'CategoriesController@index')
             ->name('api.categories.index');
 
+        // 获取话题列表
+        $api->get('topics', 'TopicsController@index')
+            ->name('api.topics.index');
+
+        // 获取指定用户话题列表
+        $api->get('users/{user}/topics', 'TopicsController@userIndex')
+            ->name('api.users.topics.index');
+
         // 需要 token 验证的接口
+
         $api->group([
             'middleware' => 'api.auth',
         ],function ($api){
+
             // 当前登录用户信息
             $api->get('user', 'UsersController@me')
                 ->name('api.user.show');
+
             // 编辑登录用户资料
             $api->patch('user', 'UsersController@update')
                 ->name('api.user.update');
+
             // 图片资源
             $api->post('images', 'ImagesController@store')
                 ->name('api.images.store');
+
             // 发布话题
             $api->post('topics', 'TopicsController@store')
                 ->name('api.topics.store');
+
             // 修改话题
             $api->patch('topics/{topic}', 'TopicsController@update')
                 ->name('api.topics.update');
+
             //删除话题
             $api->delete('topics/{topic}', 'TopicsController@destroy')
                 ->name('api.topics.delete');
+
         });
     });
 });
