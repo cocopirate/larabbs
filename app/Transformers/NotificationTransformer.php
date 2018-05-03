6367722ee50e94c40677ee206e4db3c5cc55db56
DJDLee <l353861350@163.com>
2018-05-04 01:27:30 +0800

    notifications index

---
+++ app/Transformers/NotificationTransformer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: coco
+ * Date: 2018/5/4
+ * Time: 上午10:13
+ */
+
+namespace App\Transformers;
+
+
+use League\Fractal\TransformerAbstract;
+use Illuminate\Notifications\DatabaseNotification;
+
+class NotificationTransformer extends TransformerAbstract
+{
+    public function transform(DatabaseNotification $notification)
+    {
+        return [
+            'id' => $notification->id,
+            'type' => $notification->type,
+            'data' => $notification->data,
+            'read_at' => $notification->read_at ? $notification->read_at->toDateTimeString() : null,
+            'created_at' => $notification->created_at->toDateTimeString(),
+            'updated_at' => $notification->updated_at->toDateTimeString(),
+        ];
+    }
+}
