d87e78b676f17dd70c00d13f569132a7b6fde074
DJDLee <l353861350@163.com>
2018-05-07 16:46:06 +0800

    update all

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -66,8 +66,8 @@ function model_plural_name($model){
 function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
+        
         $url = parse_url(getenv("DATABASE_URL"));
-
         return $db_config = [
             'connection' => 'pgsql',
             'host' => $url["host"],
