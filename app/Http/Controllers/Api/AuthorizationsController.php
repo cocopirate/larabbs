3b0e3fa3e50c3af21a5473412c9debc74d61bb04
DJDLee <l353861350@163.com>
2018-05-08 13:50:34 +0800

    social login change to Passport

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -12,10 +12,12 @@ use League\OAuth2\Server\AuthorizationServer;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response as Psr7Response;
+use App\Traits\PassportToken;
 
 
 class AuthorizationsController extends Controller
 {
+    use PassportToken;
 
     public function store(AuthorizationRequest $originRequest, AuthorizationServer $server, ServerRequestInterface $serverRequest)
     {
@@ -94,9 +96,14 @@ class AuthorizationsController extends Controller
                 break;
         }
 
+        $result = $this->getBearerTokenByUser($user, '1', false);
+        return $this->response->array($result)->setStatusCode(201);
+        // JWT的第三方登录token分配
+        /*
         $token = Auth::guard('api')->fromUser($user);
 
         return $this->respondWithToken($token)->setStatusCode(201);
+        */
     }
 
 
